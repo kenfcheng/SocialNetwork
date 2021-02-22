155d2f3de483c732d3147375b1afdc2c5e303e38
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2021-02-21 19:25:21 -0500

    created navbar

--- social-network/src/pages/home/index.js
+++ social-network/src/pages/home/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-
 import { SignInBtn } from "../../components";
+import { Navbar } from "../../containers";
+// Style
+import "./style.css";
 export default function Home() {
   return (
     <div className="home">
+      <Navbar />
       <SignInBtn />
     </div>
   );
