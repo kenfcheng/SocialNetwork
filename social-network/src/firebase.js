8d6544237596b1d61ef413271ba645f9d6b5a65c
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2021-02-23 11:39:38 -0500

    installed firebase

---
+++ social-network/src/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAhOjpG45aKliUa_n_lJWyLc2NyhWm3P84",
+  authDomain: "react-social-network-322fb.firebaseapp.com",
+  projectId: "react-social-network-322fb",
+  storageBucket: "react-social-network-322fb.appspot.com",
+  messagingSenderId: "690571250637",
+  appId: "1:690571250637:web:43b1a812a7dfbb40de5284",
+  measurementId: "G-WPF8SJT06K",
+};
