ae5671e0ad7b17f379dd97de681214fb6bb6040f
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2021-03-27 11:09:07 -0400

    Starting over

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import { UserContext } from "./contexts/user";
-import Home from "./pages/home";
 
 function App() {
   return (
-    <UserContext.Provider>
-      <div className="App">
-        <Home />
-      </div>
-    </UserContext.Provider>
+    <div className="App">
+      <h1>Social Media Testing</h1>
+      {/* */}
+
+      {/* Post */}
+      {/* Post */}
+    </div>
   );
 }
 
--- social-network/src/containers/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Navbar } from "./navbar";
-export { default as CreatePost } from "./create-post";
