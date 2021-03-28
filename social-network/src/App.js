a3c055041808da8ae7e97fa47ae7f0a9974f279e
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2021-03-28 12:39:06 -0400

    redid Firebase.js, also created useEffect to handle the database created with Firestore db.

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./components/post/post.jsx";
+import { db } from "./firebase";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -18,10 +19,17 @@ function App() {
     },
   ]);
 
+  // useEffect runs a piece of code based on a specific condition.
+  useEffect(() => {
+    // code runs here!
+    db.collection("posts").onSnapshot((snapshot) => {});
+    // every new posts made, snapshot will send it straight to db as soon as photo is taken.
+  }, []);
+
   return (
     <div className="app">
       <div className="app_header">
-        {/* Placeholder until I get a real logo*/}
+        {/* Instagram logo Placeholder until I get a real logo*/}
         <img
           className="app_headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
--- social-network/src/firebase.js
+++ social-network/src/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyAhOjpG45aKliUa_n_lJWyLc2NyhWm3P84",
   authDomain: "react-social-network-322fb.firebaseapp.com",
   projectId: "react-social-network-322fb",
@@ -8,13 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "690571250637",
   appId: "1:690571250637:web:43b1a812a7dfbb40de5284",
   measurementId: "G-WPF8SJT06K",
-};
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+});
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
-const provider = new firebase.auth.GoogleAuthProvider();
+// const provider = new firebase.auth.GoogleAuthProvider();
 
-export { db, auth, provider, storage };
+export { db, auth, storage };
